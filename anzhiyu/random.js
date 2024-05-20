ae488f45d47ab113fcb751fce5b634f147ed407b
panther <gincoder@aliyun.com>
2024-05-20 18:02:21 +0800

    Site updated: 2024-05-20 18:02:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/16/backend/batchInsert/","2024/05/16/backend/redisson/"];function toRandomPost(){
+var posts=["2024/05/20/arary/Explain/","2024/05/16/backend/batchInsert/","2024/05/16/backend/redisson/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
