f5bf875068d3d9a0b147d2475df529622c17c489
panther <gincoder@aliyun.com>
2024-06-13 20:59:04 +0800

    Site updated: 2024-06-13 20:59:03

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/05/20/arary/Explain/","2024/05/16/backend/batchInsert/","2024/05/16/backend/redisson/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
